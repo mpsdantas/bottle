d4c6754cdaea7feed9166b64863512f25f82a656
Marcus Dantas <marcus.dantas@mercadolivre.com>
2024-07-29 22:29:18 -0300

    Add retry

--- pkg/pubsub/topic.go
+++ pkg/pubsub/topic.go
@@ -3,6 +3,7 @@ package pubsub
 import (
 	"context"
 	"encoding/json"
+	"time"
 
 	"cloud.google.com/go/pubsub"
 	"github.com/mpsdantas/bottle/pkg/log"
@@ -26,23 +27,29 @@ func (t *topic) Publish(ctx context.Context, event string, value interface{}) {
 			return
 		}
 
-		result := t.tp.Publish(c, &pubsub.Message{
-			Data: data,
-			Attributes: map[string]string{
-				"x-event": event,
-			},
-		})
+		for i := 0; i < 100; i++ {
+			result := t.tp.Publish(c, &pubsub.Message{
+				Data: data,
+				Attributes: map[string]string{
+					"x-event": event,
+				},
+			})
 
-		id, err := result.Get(c)
-		if err != nil {
-			log.Error(c, "could not publish message data",
-				log.Err(err),
+			id, err := result.Get(c)
+			if err != nil {
+				log.Error(c, "could not publish message data",
+					log.Err(err),
+				)
+
+				time.Sleep(1 * time.Second)
+				continue
+			}
+
+			log.Info(c, "message published successfully",
+				log.String("id", id),
 			)
-			return
-		}
 
-		log.Info(c, "message published successfully",
-			log.String("id", id),
-		)
+			break
+		}
 	}(WithoutCancel(ctx))
 }
