28e67a6c03d85f1a5b0306b82c15e0e2102843f5
Marcus Dantas <mpsdantas15@gmail.com>
2026-02-09 23:52:43 -0300

    remove helpers

--- pkg/web/errors.go
+++ pkg/web/errors.go
@@ -1,34 +1,8 @@
 package web
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 )
-
-func NotFound(msg string) error {
-	return fiber.NewError(fiber.StatusNotFound, msg)
-}
-
-func Internal(msg string) error {
-	return fiber.NewError(fiber.StatusInternalServerError, msg)
-}
-
-func Validation(msg string) error {
-	return fiber.NewError(fiber.StatusBadRequest, msg)
-}
-
-func FailureCause(cause string, msg string) error {
-	return fiber.NewError(fiber.StatusUnprocessableEntity, fmt.Sprintf("%v: %v", cause, msg))
-}
-
-func Unauthorized(msg string) error {
-	return fiber.NewError(fiber.StatusUnauthorized, msg)
-}
-
-func Forbidden(msg string) error {
-	return fiber.NewError(fiber.StatusForbidden, msg)
-}
 
 func Error(code int, text string) error {
 	return fiber.NewError(code, text)
